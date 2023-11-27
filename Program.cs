58f53d3f95f35c2e66cb24d638c2052023ec4a39
Andy Arias <146486695+andyarias30@users.noreply.github.com>
2023-11-27 10:47:39 -0500

    account balance

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Console.WriteLine("Hello, World!");
+
+int age = 20;
+// age = "twenty"; //cant do this
+
+string name = "Andy";
+//Console.WriteLine($"your name is:  {name} and you're {age} years old");
+BankAccount newAccount = new BankAccount();
+newAccount.Balance = 100.99;
+
+Console.WriteLine($"Your balance is {newAccount.Balance}");
+
+
+
